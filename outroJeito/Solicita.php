ea4cfe7d65187360a40a6d7518e3fd4ca7953bca
Allan Rocha <73902180+Allankrc@users.noreply.github.com>
2023-10-05 17:02:07 -0300

    Alterações para um novo caminho

---
+++ outroJeito/Solicita.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    
+
+    <title>Document1</title>
+</head>
+<body>
+    <?php 
+$dir_ref = 75;
+
+$dir_hot = 100;	
+?>
+
+
+
+
+    <label> $Nome</label>
+    	<input type="text" name="nome"></input> <br>
+    <label>$Reg </label>	
+    <select name="estado' id="estado> 
+<option value="0" > Escolha</option>
+<option value="1" > ESTADO 1</option>
+<option value="2" > ESTADO 2</option>
+<option value="3" > ESTADO 3</option>
+<option value="4" > ESTADO 4</option>
+<option value="5" > ESTADO 5</option>
+<option value="6" > ESTADO 6</option>
+<option value="7" > ESTADO 7</option>
+<option value="8" > ESTADO 8</option>
+<option value="9" > ESTADO 9</option>
+<option value="10" > ESTADO 10</option>
+<option value="11" > ESTADO 11</option>
+<option value="12" > ESTADO 12</option>
+<option value="13" > ESTADO 13</option>
+<option value="14" > ESTADO 14</option>
+<option value="15" > ESTADO 15</option>
+<option value="16" > ESTADO 16</option>
+<option value="17" > ESTADO 17</option>
+<option value="18" > ESTADO 18</option>
+<option value="19" > ESTADO 19</option>
+<option value="20" > ESTADO 20</option>
+<option value="21" > ESTADO 21</option>
+<option value="22" > ESTADO 22</option>
+<option value="23" > ESTADO 23</option>
+<option value="24" > ESTADO 24</option>
+<option value="25" > ESTADO 25</option>
+<option value="26" > ESTADO 26</option>
+    </select> <br>
+
+
+    <label>$Tipo</label>
+        <select name="tipo' id="tipo> 
+<option value="0" > Escolha</option>
+<option value="1" > Superintendente</option>
+<option value="2" > CLT Veículo empresa</option>
+<option value="3" > CLT Veículo próprio</option>
+<option value="4" > OUTRO</option>
+</select> <br>
+
+
+    <label>$Viagem_N</label>	
+    <br>
+    <label>$D_sol</label>	
+    <br>
+    <label> $D_saida</label>	
+    <br>
+    <label>$D_chega </label>	
+    <br>
+    <label> $Tempo</label>
+    <br>
+    <label>  $Prev_hosp	</label>	
+    <br>
+    <label> $Prev_ref</label>	
+    <br>
+    <label> $Prev_com</label>	
+    <br>
+    <label> $Prev_km</label>	
+    <br>
+    <label> $Prev_out	</label>	
+    <br>
+    <label>  $Prev_total	</label>	
+    <br>
+    <label>$Vcode (PRIMARY KEY)</label>	
+    <br>
+    <label>$Array_cid</label>
+    <select name="cidade' id="cidade> 
+<option value="0" > Escolha</option>
+<option value="1" > cidade 1</option>
+<option value="2" > cidade 2</option>
+    </select>
+
+
+
+
+
+    <button type="button" class="btn btn-send">Enviar</button>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</body>
+</html>
